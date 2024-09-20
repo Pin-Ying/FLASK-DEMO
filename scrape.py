b20908ab14ffcc9b4b02c09ca45ddcdaabf64f44
PinLing <56640582+Pin-Ying@users.noreply.github.com>
2024-09-20 21:16:03 +0800

    fix bugs(scrape.py)

--- scrape.py
+++ scrape.py
@@ -30,7 +30,9 @@ def scrape_pm25(sort=False, ascending=True):
         url = "https://data.moenv.gov.tw/api/v2/aqx_p_02?api_key=e8dd42e6-9b8b-43f8-991e-b3dee723a52d&limit=1000&sort=datacreationdate%20desc&format=JSON"
         datas = requests.get(url).json()["records"]
         df = pd.DataFrame(datas)
-        df["pm25"] = df["pm25"].apply(lambda x: eval(x))
+        df = df[df["pm25"]!=""]
+        df["pm25"] = df["pm25"].apply(lambda x: eval(x) if x else 0)
+
         if sort:
             df = df.sort_values("pm25", ascending=ascending)
 
@@ -53,4 +55,4 @@ def get_pm25_json():
 
 
 if __name__ == "__main__":
-    print(get_pm25_json())
+    print(scrape_pm25())
